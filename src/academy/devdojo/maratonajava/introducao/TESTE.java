573af49f50f142dd91ff358be70da136b82a77af
mateushongjin <mateushongjin@hotmail.com>
2025-03-06 20:17:00 -0300

    AULA COMPLETA 35 DIA 06/03/2025 RETIREI teste.java

--- src/academy/devdojo/maratonajava/introducao/TESTE.java
+++ src/academy/devdojo/maratonajava/introducao/TESTE.java
@@ -4,26 +4,10 @@ import java.util.Arrays;
 
 public class TESTE {
     public static void main(String[] args) {
-        String[] nomesAntigos = {"Ana", "Bruno", "Carlos"};
+        int[] numeros = {10, 20, 30, 40, 50};
 
-        // Criando um novo array maior
-        String[] nomesNovos = new String[5];
-
-        // Copiando os valores antigos manualmente
-        for (int i = 0; i < nomesAntigos.length; i++) {
-            nomesNovos[i] = nomesAntigos[i];
-        }
-
-        // Adicionando novos valores
-        nomesNovos[3] = "Daniela";
-        nomesNovos[4] = "Eduardo";
-
-        // Atualizando a referência para o novo array
-        nomesAntigos = nomesNovos;
-
-        // Exibindo os valores
-        for (String nome : nomesAntigos) {
-            System.out.println(nome);
+        for (int num : numeros) {
+            System.out.println("Número: " + num);
         }
     }
 }
